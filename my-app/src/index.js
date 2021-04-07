5c2df95a4cd522f591eb31e68fc527de35308ff1
tanay123456789 <tanaydwivedi456@gmail.com>
2021-04-07 09:25:19 +0530

    refactor:initialise losing array

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
+
+
+
+// array to display message of losing
+static loser=[
+    'Tumse Na ho payenga',
+    'Stop gambling your hard earned money',
+    '25 din mein paise kabhi double na hopayenge tere',
+    'Biggest Loser',
+    'Acche din na aayenga tumahare',
+    'You can never be a billionaire'
+]
 ReactDOM.render(
 
     <App />,document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
