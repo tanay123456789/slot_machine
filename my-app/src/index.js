811a000e6bba593073a29bacaf0fb02d521e8a89
tanay123456789 <tanaydwivedi456@gmail.com>
2021-04-18 13:43:47 +0530

    feat:added losing function

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,14 +6,20 @@ import './index.css';
 
 
 // array to display message of losing
-static loser=[
-    'Tumse Na ho payenga',
-    'Stop gambling your hard earned money',
-    '25 din mein paise kabhi double na hopayenge tere',
-    'Biggest Loser',
-    'Acche din na aayenga tumahare',
-    'You can never be a billionaire'
-]
+function defeat(){
+    static loser=[
+        'Tumse Na ho payenga',
+        'Stop gambling your hard earned money',
+        '25 din mein paise kabhi double na hopayenge tere',
+        'Biggest Loser',
+        'Acche din na aayenga tumahare',
+        'You can never be a billionaire',
+        'Once a loser always a loser',
+        'Tata bye bye khatam gya'
+    ]
+
+}
+
 ReactDOM.render(
 
     <App />,document.getElementById('root')
