ed955948aab7388e63e5b6b72f628ca0f749404c
tanay123456789 <tanaydwivedi456@gmail.com>
2021-04-23 12:21:29 +0530

    refactor:created audio and images files

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,16 +5,20 @@ import './index.css';
 
 //function to emit sound when person enters the coin 
 function casinoSound(){
+    return (
+        <audio autoplay="autoplay" className="player" preload="false">
+          <source src="coin.mp3" />
+        </audio>  
+        );
 
 }
 // function to display message of losing
 function defeat(){
     const loser=[
-        'Tumse Na ho payenga',
+        'Beta,Tumse Na ho payenga',
         'Stop gambling your hard earned money',
         '25 din mein paise kabhi double nhi hopayenge tumhare',
         'Biggest Loser',
-        'Acche din na aayenga tumahare',
         'You can never be a billionaire',
         'Once a loser always a loser',
         'Tata bye bye goodbye khatam gya'
@@ -25,12 +29,12 @@ function defeat(){
 function winningSound(){
     return (
         <audio autoplay="autoplay" className="player" preload="false">
-          <source src="https://andyhoffman.codes/random-assets/img/slots/winning_slot.wav" />
+          <source src="./assests/audio/winningSound.mp3" />
         </audio>  
         );
 }
 class App extends React.Component{
-    
+
 }
 
 ReactDOM.render(
